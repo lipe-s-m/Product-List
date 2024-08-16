2cd0f2034ac87fe4fb27e99a6b17cb836a8bf7ac
lipe-s-m <felipe.s.m1609@gmail.com>
2024-08-16 05:29:13 -0300

    feat: add javascript integration add and remove itens of product list

---
+++ assets/js/pages/index/modificar-carrinho.js
@@ -0,0 +1,33 @@
+import DataManager from "./script.js";
+
+export function incrementarUnidadeProduto(product, newProduct) {
+    DataManager.adicionarProduto(newProduct);
+  const qntProdutosLabel = document
+    .getElementById(product)
+    .querySelector(".modificar-carrinho")
+    .querySelector("span");
+  newProduct.unidadeProduto++;
+  qntProdutosLabel.textContent = newProduct.unidadeProduto;
+}
+
+export function decrementarUnidadeProduto(product, newProduct) {
+    DataManager.removerProduto(newProduct);
+  const qntProdutosLabel = document
+    .getElementById(product)
+    .querySelector(".modificar-carrinho")
+    .querySelector("span");
+  if (newProduct.unidadeProduto > 1) {
+    newProduct.unidadeProduto--;
+    qntProdutosLabel.textContent = newProduct.unidadeProduto;
+  } else if (newProduct.unidadeProduto === 1) {
+    newProduct.unidadeProduto--;
+    const qntProdutos = document
+      .getElementById(product)
+      .querySelector(".modificar-carrinho");
+    qntProdutos.classList.add("hidden");
+    const cardAddProduto = document
+      .getElementById(product)
+      .querySelector(".adicionar-carrinho");
+    cardAddProduto.hidden = false;
+  }
+}
---
+++ assets/js/pages/index/script.js
@@ -0,0 +1,97 @@
+import {
+  decrementarUnidadeProduto,
+  incrementarUnidadeProduto,
+} from "./modificar-carrinho.js";
+
+class Carrinho {
+  constructor() {
+    if (Carrinho.instance) return Carrinho.instance;
+    this.produtos = [];
+    Carrinho.instance = this;
+  }
+
+  adicionarProduto(produto) {
+    this.produtos.push(produto);
+    console.log(this.produtos);
+  }
+  removerProduto(produto) {
+    const index = this.produtos.findIndex((product) => {
+      return product.nomeProduto === produto.nomeProduto;
+    });
+
+    if (index !== -1) {
+      this.produtos.splice(index, 1);
+    }
+
+    console.log(this.produtos);
+  }
+  obterProdutos() {
+    return this.produtos;
+  }
+}
+
+const DataManager = new Carrinho();
+export default DataManager;
+var i = 0;
+
+window.adicionarProduto = function (product) {
+  const cardAddProduto = document
+    .getElementById(product)
+    .querySelector(".adicionar-carrinho");
+  const nomeProduto = document
+    .getElementById(product)
+    .querySelector(".descricao-sobremesa")
+    .querySelectorAll("p")[0].textContent;
+  const descProduto = document
+    .getElementById(product)
+    .querySelector(".descricao-sobremesa")
+    .querySelectorAll("p")[1].textContent;
+  const precoProduto = document
+    .getElementById(product)
+    .querySelector(".descricao-sobremesa")
+    .querySelectorAll("p")[2].textContent;
+  const imgProduto = document.getElementById(product).querySelector("img");
+  const newProduct = {
+    nomeProduto,
+    descProduto,
+    precoProduto,
+    imgProduto,
+    unidadeProduto: 1,
+  };
+
+  DataManager.adicionarProduto(newProduct);
+
+  const count = 1;
+
+  const qntProdutos = document
+    .getElementById(product)
+    .querySelector(".modificar-carrinho");
+
+  let addProductImage = document.createElement("img");
+  addProductImage.src = "assets/images/icon-increment-quantity.svg";
+  addProductImage.alt = "Adicionar uma unidade ao carrinho";
+  addProductImage.addEventListener("click", function () {
+    incrementarUnidadeProduto(product, newProduct);
+  });
+
+  let removeProductImage = document.createElement("img");
+  removeProductImage.src = "assets/images/icon-decrement-quantity.svg";
+  removeProductImage.alt = "Remover uma unidade do carrinho";
+  removeProductImage.addEventListener("click", function () {
+    decrementarUnidadeProduto(product, newProduct);
+  });
+
+  let texto = document.createTextNode(`${count}`);
+  let elementoTexto = document.createElement("span");
+  elementoTexto.appendChild(texto);
+
+  cardAddProduto.hidden = true;
+  qntProdutos.classList.remove("hidden");
+
+  qntProdutos.innerHTML = "";
+  qntProdutos.appendChild(removeProductImage);
+  qntProdutos.appendChild(elementoTexto);
+  qntProdutos.appendChild(addProductImage);
+
+  i++;
+};
